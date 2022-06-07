dee6c27ff5595e74a7d1a133dbc16e3fd67e23aa
imtiyazz24 <imtiyaz.ansari24@gmail.com>
2022-06-07 10:49:21 +0530

    y

--- src/routes/route.js
+++ src/routes/route.js
@@ -63,11 +63,42 @@ router.post("/test-post-3", function(req, res) {
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+
+
+router.get("/sol1", function(req, res) {
+    let arr= [1,2,3,5,6,7]
+ 
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+ 
+   let lastDigit= arr.pop()
+   let consecutiveSum= lastDigit * (lastDigit+1) / 2
+   let missingNumber= consecutiveSum - total
+ 
+   res.send(  { data: missingNumber  }  );
+ });
+ 
+
+ 
+    router.get("/sol2", function (req, res) {
+        let arr= [33, 34, 35, 37, 38]
+        let len= arr.length
+      
+        let total = 0;
+        for (var i in arr) {
+            total += arr[i];
+        }
+      
+        let firstDigit= arr[0]
+        let lastDigit= arr.pop()
+        let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+        let missingNumber= consecutiveSum - total
+       
+        res.send(  { data: missingNumber  }  );
+      });
+    
+
 
 module.exports = router;
